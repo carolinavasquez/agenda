947bed1394a7e63b0cb01ff5562073ee4aff07c7
Alumno <alumno@localhost.localdomain>
2014-03-24 08:16:44 -0600

    first commit

---
+++ agenda.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class agenda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner agenda= new Scanner(System.in);
+
+		int arr[] = new  int [5];
+		
+		System.out.println("Ingrese el numero telefonico");
+		arr[0]= agenda.nextInt();
+		System.out.println("Ingrese el numero telefonico");
+		arr[1]= agenda.nextInt();
+		System.out.println("Ingrese el numero telefonico");
+		arr[2]= agenda.nextInt();
+		System.out.println("Ingrese el numero telefonico");
+		arr[3]= agenda.nextInt();
+		System.out.println("Ingrese el numero telefonico");
+		arr[4]= agenda.nextInt();
+	
+			System.out.println("☎☎☎agenda telefonica☎☎☎ ");
+			for (int i=0; i<arr.length; i++){
+				System.out.println(arr[i]);
+			{
+		}
+}		
+}
+}
